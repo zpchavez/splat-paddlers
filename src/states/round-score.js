49c571313d77805f5b7936970e8afb061a43e5e8
Zachary Chavez <zpchavez@gmail.com>
2017-08-26 09:32:12 -0700

    Align round scores and sort by highest first

--- src/states/round-score.js
+++ src/states/round-score.js
@@ -2,6 +2,18 @@ import TextUtil from '../text-util';
 import gameState from './game';
 import { getPlayerControls } from '../controls';
 
+const fill = function(len) {
+  return (Array(Math.max(0, len)).fill(' ')).join('');
+}
+
+const rpad = function(str, len) {
+  return str + fill(len - str.length);
+}
+
+const lpad = function(str, len) {
+  return fill(len - str.length) + str;
+}
+
 module.exports = (g) => {
   let score = {};
   g.collisionGroups.paddles.forEach(paddle => {
@@ -14,6 +26,20 @@ module.exports = (g) => {
   })
   g.things.forEach(thing => g.remove(thing.sprite));
 
+  // Sort score descending
+  const sortedScores = Object.keys(score).map(
+    color => ({ color, score: score[color]})
+  ).sort((a, b) => b.score - a.score);
+
+  const longestColor = sortedScores.reduce(
+    (acc, score) => score.color.length > acc ? score.color.length : acc,
+    sortedScores[0].color.length
+  );
+  const longestScore = sortedScores.reduce(
+    (acc, score) => score.score.toString().length > acc ? score.score.toString().length : acc,
+    sortedScores[0].score.toString().length
+  );
+
   const textUtil = new TextUtil(g);
   const scoreTexts = [];
   scoreTexts.push(
@@ -24,10 +50,13 @@ module.exports = (g) => {
       128
     )
   );
-  const scoreTextStrings = Object.keys(score).map(color => `${color}: ${score[color]}`);
+
+  const scoreTextStrings = sortedScores.map(
+    ({ score, color }) => rpad(`${color}`, longestColor + 1) + lpad(`${score}`, longestScore)
+  );
   scoreTexts.push.apply(
     scoreTexts,
-    textUtil.createHorizontallyCenteredTexts(scoreTextStrings, 36, '#000000', 192, 32)
+    textUtil.createHorizontallyCenteredTexts(scoreTextStrings, 36, '#000000', 192, 48)
   );
 
   g.wait(3000, () => {
